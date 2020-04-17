8202b55fb6a55f1c9a57359bb2c7292b3ccfc9be
Emiya·Leon <wangs.coder@gmail.com>
2020-04-17 11:36:30 +0800

    feat: 首页

--- src/config/index.ts
+++ src/config/index.ts
@@ -16,7 +16,8 @@ const HTTP_ERROR = {
   504: '网关超时。'
 };
 if (IS_DEV) {
-  baseUrl = 'http://localhost:3000';
+  // baseUrl = 'http://192.168.1.72:3000';
+  baseUrl = 'http://172.16.3.123:3000';
   noConsole = false;
 } else {
   baseUrl = 'https://binaryify.github.io/NeteaseCloudMusicApi';
