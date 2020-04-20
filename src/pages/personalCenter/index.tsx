091523a202725cbf5cdb775424515ede9b92e12c
wangs1203 <15201670063@163.com>
2020-04-20 14:10:33 +0800

    feat：common model

--- src/pages/personalCenter/index.tsx
+++ src/pages/personalCenter/index.tsx
@@ -10,9 +10,9 @@ import {
 } from '@tarojs/components';
 import { connect } from '@tarojs/redux';
 // import {
-//   AtTabs,
-//   AtTabsPane
-//   // AtIcon
+// AtNoticebar
+// AtTabsPane
+// AtIcon
 // } from 'taro-ui';
 // import { IndexEffectType } from './model';
 import './index.scss';
@@ -20,6 +20,7 @@ import './index.scss';
 interface PageOwnProps {}
 
 interface PageStateProps {
+  common:any;
 }
 
 interface PageDispatchProps {
@@ -29,12 +30,20 @@ interface PageState {
 }
 
 type IProps = PageStateProps & PageDispatchProps & PageOwnProps;
+@connect(
+  ({ common }) => ({
+    common
+  }),
+  (dispatch) => {
 
+  }
+)
 class PersonCenter extends Component<IProps, PageState> {
   public config: Config = {
     navigationBarTitleText: '我的'
   }
 
+  // eslint-disable-next-line no-useless-constructor
   public constructor (...rest) {
     super(...rest);
   }
@@ -47,9 +56,11 @@ class PersonCenter extends Component<IProps, PageState> {
 
   public render (): JSX.Element {
     return (
-      <View className="">
-        wode
+      <View>
+        test
       </View>
     );
   }
 }
+
+export default PersonCenter;
