680ee412684b707ad2da558d9d3e9f4fe0e7636a
wangs1203 <15201670063@163.com>
2020-06-29 18:40:02 +0800

    feat: 添加接口，eslint规则

--- src/models/common.ts
+++ src/models/common.ts
@@ -2,17 +2,34 @@ import Taro from '@tarojs/taro';
 import { Model } from 'dva-core';
 import modelExtend from 'dva-model-extend';
 import { model } from '@utils/model';
+import {
+  fetchCheckMusic,
+  fetchCheckMusicParam
+} from '@/services/common';
 
+export { fetchCheckMusicParam };
 export const enum CommonEffectType {
-
+  getCheckMusic = 'common/getCheckMusic',
 }
+
 export default modelExtend(model, {
   namespace: 'common',
   state: {
     userInfo: Taro.getStorageSync('userInfo'),
     userId: ''
   },
+  effects: {
 
+    * getCheckMusic ({ payload }:{payload:fetchCheckMusicParam}, { call, put, select }) {
+      const res = yield call(fetchCheckMusic, payload);
+      console.log(res);
+      if(res.isOk){
+        Taro.navigateTo({
+          url: `/pages/songDetail/index?id=${payload.id}`
+        });
+      }
+    }
+  },
   reducers: {
     updateUserInfo (state, { payload }) {
       if (payload) {
